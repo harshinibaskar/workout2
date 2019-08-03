2adf89c2977181934e8525a472fc3359b8e42e53
harshinibaskar <51949520+harshinibaskar@users.noreply.github.com>
2019-08-03 14:37:06 +0530

    Update 6pro.py

--- 6pro.py
+++ 6pro.py
@@ -1,9 +1,9 @@
 s1=int(input())
 s2=list(map(int,input().split()))
-go=0
+s3=0
 for one1 in range(s1):
     for two2 in range(one1,s1):  
         for three3 in range(two2,s1):
             if s2one1]<s2two2]<s2three3]:
-                go+=1
-print(go)
+                s3+=1
+print(s3)
